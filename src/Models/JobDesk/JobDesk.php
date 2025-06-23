4cda0d6a93aca609113a820ea762f52c9b7b5d0b
Hamzah <hamzahnafalah@gmail.com>
2025-06-23 13:51:11 +0700

    update modules

--- src/Models/JobDesk/JobDesk.php
+++ src/Models/JobDesk/JobDesk.php
@@ -2,9 +2,6 @@
 
 namespace Hanafalah\ModuleProfession\Models\JobDesk;
 
-use Hanafalah\ModuleProfession\{
-    Enums\Profession\Flag
-};
 use Hanafalah\ModuleProfession\Models\Occupation\Occupation;
 use Hanafalah\ModuleProfession\Resources\JobDesk\{
     ViewJobDesk, ShowJobDesk
@@ -14,27 +11,11 @@ class JobDesk extends Occupation
 {
     protected $table = 'unicodes';
 
-    protected static function booting(): void{
-        static::setFlags(Flag::JOB_DESK->value);
-    }
-
-    protected static function booted(): void{
-        parent::booted();
-
-        static::creating(function ($query) {
-            if (!isset($query->flag)) $query->flag = Flag::JOB_DESK->value;
-        });
-    }
-
     public function getViewResource(){
         return ViewJobDesk::class;
     }
 
     public function getShowResource(){
         return ShowJobDesk::class;
-    }
-
-    public function childs(){
-        return $this->hasManyModel('JobDesk','parent_id')->where('flag',Flag::JOB_DESK->value);
     }
 }
--- src/Models/Occupation/Occupation.php
+++ src/Models/Occupation/Occupation.php
@@ -4,7 +4,6 @@ namespace Hanafalah\ModuleProfession\Models\Occupation;
 
 use Hanafalah\ModuleProfession\{
     Models\Profession\Profession,
-    Enums\Profession\Flag
 };
 use Hanafalah\ModuleProfession\Resources\Occupation\ShowOccupation;
 use Hanafalah\ModuleProfession\Resources\Occupation\ViewOccupation;
@@ -12,10 +11,6 @@ use Hanafalah\ModuleProfession\Resources\Occupation\ViewOccupation;
 class Occupation extends Profession
 {
     protected $table = 'unicodes';
-
-    public static function getFlag(): string{
-        return Flag::OCCUPATION->value;
-    }
 
     public function getViewResource(){
         return ViewOccupation::class;
